aaeddc0b94aad8c774b43514afefab3cf010e77e
robinlh <robinh@MacBook-Pro.local>
2021-10-13 16:27:46 +0200

    comments for the connection string.

--- py_app/test_db.py
+++ py_app/test_db.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine
 
+# connection string components:
+# connect to db as root user with password:"password" - root:password
+# connect to mysql db host on standard port - @mysql:3306
+# connect to database test - /test
 mysql_conn_str = "mysql+pymysql://root:password@mysql:3306/test"
 engine = create_engine(mysql_conn_str)
 connection = engine.connect()
