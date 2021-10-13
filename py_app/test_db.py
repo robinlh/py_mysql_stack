5e55f16b67439ba37f873efa2161fdb764ea397b
robinlh <robinh@MacBook-Pro.local>
2021-10-13 15:20:41 +0200

    basic python mysql stack.

---
+++ py_app/test_db.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+
+mysql_conn_str = "mysql+pymysql://root:password@mysql:3306/test"
+ngine = create_engine(mysql_conn_str)
+engine = create_engine(mysql_conn_str)
+connection = engine.connect()
+q = connection.execute('SHOW DATABASES')
+available_tables = q.fetchall()
+print(available_tables)
